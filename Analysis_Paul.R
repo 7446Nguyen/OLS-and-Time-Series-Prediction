65342c08a7a2a097e121aa9ea38b8bba19878d3d
PaulAdams4361 <paula@smu.edu>
2019-09-26 08:01:50 -0500

    Updating Analysis_Paul OLS var names

--- Analysis_Paul.R
+++ Analysis_Paul.R
@@ -350,9 +350,9 @@ model.forward.Start <- lm(log(price_doc)~1,data = df)
 model.Allvar <- lm(log(price_doc) ~ id + timestamp + full_sq +	life_sq + floor + max_floor + material + build_year + num_room + kitch_sq + product_type
                     + raion_popul + green_zone_part + indust_part + children_preschool + preschool_quota + children_school + hospital_beds_raion
                     + healthcare_centers_raion + university_top_20_raion + shopping_centers_raion + office_raion + railroad_terminal_raion
-                    + big_market_raion + full_all + X0_6_all + X7_14_all + X0_17_all + X16_29_all + X0_13_all + build_count_block + build_count_wood 
-                    + build_count_frame + build_count_brick + build_count_before_1920 + build_count_1921-1945 + build_count_1946-1970
-                    + build_count_1971-1995 + build_count_after_1995 + metro_min_avto + metro_km_avto + metro_min_walk + metro_km_walk + school_km
+                    + big_market_raion + full_all + `0_6_all` + `7_14_all` + `0_17_all` + `16_29_all` + `0_13_all` + build_count_block + build_count_wood 
+                    + build_count_frame + build_count_brick + build_count_before_1920 + `build_count_1921-1945` + `build_count_1946-1970`
+                    + `build_count_1971-1995` + build_count_after_1995 + metro_min_avto + metro_km_avto + metro_min_walk + metro_km_walk + school_km
                     + park_km + green_zone_km + industrial_km + railroad_station_walk_km + railroad_station_walk_min + ID_railroad_station_walk
                     + railroad_station_avto_km + railroad_station_avto_min + public_transport_station_km + public_transport_station_min_walk
                     + kremlin_km + big_road1_km + big_road2_km + railroad_km + bus_terminal_avto_km + big_market_km + market_shop_km + fitness_km
